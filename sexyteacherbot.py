b43cbac48723bb31738f4b08e278a17893470733
Case <c4645843@trbvn.com>
2017-05-29 12:15:21 +0000

    refactor by Woz, thanks dude

--- sexyteacherbot.py
+++ sexyteacherbot.py
@@ -1,14 +1,9 @@
-#!/usr/bin/env python2
-from Queue import Queue
-import platform
-import hashlib
+import json
 import random
-import thread
-import string
-import socket
+import threading
 import time
-import ssl
-import re
+
+from Bot import Bot
 
 art = """
    _____              _______              _               ____        _
@@ -19,231 +14,157 @@ art = """
  |_____/ \___/_/\_\\__, |_|\___|\__,_|\___|_| |_|\___|_|  |____/ \___/ \__|
                     __/ |
                    |___/
-                                  Special thanks to ClaudiaD & leet
+                                  Special thanks to ClaudiaD & l33t
 """
 
-# Config
-config = {}
-execfile("configuration.conf", config)
-version = "This bot was forked from ClaudiaMIND v0.1.3 - Special thanks to ClaudiaD and leet. https://github.com/ClaudiaDAnon/Hive"
+CONF_FILENAME = "conf.json"
 
-# Courses
-coursefile = "courses.txt"
-descriptionsfile = "descriptions.txt"
-courses = {}
-descriptions = {}
-execfile(coursefile, courses)
-execfile(descriptionsfile, descriptions)
+data = json.load(open(CONF_FILENAME, "r"))
+conf = data["conf"]
 
-nickname = "SexyTeacherBot"
-username = ""
-realname = nickname
-password = ""
+bot = Bot(data)
 
-ircd = config["ircd"]
-ircport = config["ircport"]
-admins = config["admins"]
-channel = config["channel"]
 
-# Message-sending
-def message(msg):
-    s.send("PRIVMSG " + channel + " :" + msg + "\r\n")
+def chat():
+    while True:
+        msg = input()
+        bot.message(msg, conf["chans"][0])
 
-# Private-Messaging
-def privmessage(user2, msg):
-    s.send(":source PRIVMSG " + user2 + " :" + msg + "\r\n")
 
-# Get file length
-def file_len(fname):
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
+def jew():
+    while True:
+        s = random.randint(3600 * 24, 3600 * 36)
+        time.sleep(s)
+        bot.message(data["commands"]["donate"], conf["chans"][0])
 
-# Take input from terminal
-def take_input(chan, s):
-    while 1:
-        data = raw_input()
-        send_data = (":source PRIVMSG " + channel + " :" + data + "\r\n")
-        s.send(send_data)
 
-def isWindows():
-    return platform.system() == "Windows"
+def welcome(nick):
+    greet = ("Welcome to #learninghub %s! Here you'll find lots of resources and people to learn hacking/pentesting "
+             "as well as other IT subjects. Type ?goldmine to get started and get rid of the welcome message. Type "
+             "?desc <course_number> to know the description of a course. You have to use the course number in the "
+             "ghostbin. Type ?help for more." % nick)
+    bot.notice(nick, greet)
 
-def isNewUser(user):
-    userfile = open("users.txt","r")
-    previous = []
-    for line in userfile:
-        line = line.strip('\r\n')
-        line = line.strip('\n')
-        previous += [line]
-    userfile.close()
-    return sha2(user) not in previous
 
-def saveuser(user):
-    save = open("users.txt","a")
-    save.write(sha2(user) + "\n")
-    save.close()
+def check_nick(msg, nick=None):
+    return "%s: %s" % (nick, msg) if nick else msg
 
-#Better way to not repeat the same thing over and over
-def switch(terms):
-    for term in terms:
-        if term == sentmessage:
-            return True
-    return False
 
-#Save usernames in SHA-256
-def sha2(text):
-    return hashlib.sha256(text).hexdigest()
+def halp(nick=None):
+    if nick in data["helps"]:
+        return data["helps"][nick]
 
-def pingpong(msg):
-    number = msg.strip("PING :")
-    pong = "PONG :" + number
-    s.send(pong)
+    commands = [x for x in data["commands"]]
+    commands += [x for x in data["defs"] if len(x) > 3]
+    commands.sort()
 
-def getNick(recvd):
+    cmds = ", ".join("?%s" % x for x in commands)
+    commands += [x for x in data["defs"] if len(x) > 3]
+    commands.sort()
+
+    cmds = ", ".join("?%s" % x for x in commands)
+    msg = "Available commands: \u0002%s\u000F." % cmds
+    return check_nick(msg, nick)
+
+
+def random_course(nick=None):
+    i = random.choice(data["courses"])
+    msg = "%s. %s." % (i["id"], i["title"])
+    return check_nick(msg, nick)
+
+
+def desc(course):
     try:
-        return recvd.split("!")[1].split("@")[0]
+        info = data["courses"][int(course)]
+        d = info["desc"]
+        return d
     except:
-        return None
+        return "?desc <0-108>"
 
-def rest():
+
+def link(course):
     try:
-        if ("?" == sentmessage[0]) or ("what" == sentmessage[:4]):
-            time.sleep(1)
+        info = data["courses"][int(course)]
+        url = info["link"]
+        return url
     except:
-        pass
+        return "?link <0-108>"
 
-# Aesthetics
-if isWindows():
-    class bcolors:
-        HEADER = ''
-        OKBLUE = ''
-        OKGREEN = ''
-        WARNING = ''
-        FAIL = ''
-        ENDC = ''
-        BOLD = ''
-        UNDERLINE = ''
-else:
-    class bcolors:
-        HEADER = '\033[95m'
-        OKBLUE = '\033[94m'
-        OKGREEN = '\033[92m'
-        WARNING = '\033[93m'
-        FAIL = '\033[91m'
-        ENDC = '\033[0m'
-        BOLD = '\033[1m'
-        UNDERLINE = '\033[4m'
 
-print(bcolors.OKBLUE + art + bcolors.ENDC)
-time.sleep(1)
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-try:
-    print(bcolors.OKBLUE + "Establishing connection to IRC:" + str(ircd) + "/" + str(ircport) + bcolors.ENDC)
-    s.connect((ircd, ircport))
-    s.settimeout(130)
-    s = ssl.wrap_socket(s)
-    print(bcolors.OKGREEN + "Connection established!" + bcolors.ENDC)
-except Exception as e:
-    print(bcolors.FAIL + "Failed to connect. [" + str(ircport) + "]" + bcolors.ENDC)
-    print(e)
-    exit()
-
-print(bcolors.OKBLUE + "Sending credentials. Bot nickname: " + nickname + bcolors.ENDC)
-s.send("PASS " + password + "\r\n")
-s.send("NICK " + nickname + "\r\n")
-s.send("USER " + username + " 0 * :" + realname + "\r\n")
-print(bcolors.OKBLUE + "Credentials sent. Waiting for log-in..." + bcolors.ENDC)
-
-# Wait for ping from server
-while True:
-    recvd = s.recv(4096)
-
-    if recvd == "":
-        exit(0)
-    elif "PING :" in recvd:
-        pingpong(recvd)
-    elif (nickname + "!" + username) in recvd:
-        break
-
-print bcolors.OKBLUE + "[+] Joining channel: " + channel + bcolors.ENDC
-s.send(":source PRIVMSG nickserv :IDENTIFY "+ password + "\r\n")
-s.send(":source JOIN :" + "#bots" + "\r\n")
-s.send(":source JOIN :" + channel + "\r\n")
-s.send(":source MODE " + nickname + " +B \r\n")
-
-# Loop to receive input and execute commands
-q = Queue()
-thread.start_new_thread(take_input, (channel,s,))
-
-while True:
-    recvd = s.recv(1024)
-    msg = string.split(recvd)[3:] #WHY
-    sentmessage = " ".join(msg)[1:].lower()
-    senderuser = recvd.split("!")[0][1:]
-    # Quits the program before it bugging
-    if recvd == "":
-        exit(0)
-
-    if "PING :" in recvd:
-        pingpong(recvd)
+def whatof(arg=None):
+    if arg in data["whatof"]:
+        return data["whatof"][arg]
     else:
-        print bcolors.OKBLUE + "<" + senderuser + "> " + bcolors.ENDC + sentmessage
+        return "There is no data for this user."
 
-    # TODO: implement a white input list
-    if ("JOIN :" + channel in recvd) and isNewUser(senderuser):
-      message("Welcome to #learninghub " + senderuser + "! Here you'll find lots of resources and people to learn hacking/pentesting as well as other IT subjects. Type ?goldmine to get started. Type ?desc <coursenumber> to know the description of a course. You have to use the course number in the ghostbin. Type ?help for more.")
-      saveuser(senderuser)
-    elif switch(["?rc","?randomcourse"]):
-      rand = random.randint(1,len(courses))
-      message(courses["c" + str(rand)])
-    elif switch(["?gm","?goldmine"]):
-      message("Get your computer sk1llz improved with these amazing courses! -> download -> www.ghostbin.com/paste/j858d or www.github.com/caseanon/Dump || WATCH ONLINE http://handbookproject.github.io")
-    elif switch(["?cotw","?courseoftheweek"]):
-      message(courses[checkCourse(1)])
-    elif "?desc" == sentmessage[:5]:
-        try:
-            course = sentmessage[6:]
-            message(descriptions["c" + course])
-        except Exception as e:
-            message("Input a valid course faggot")
-            print(e)
-    elif switch(["?v","?version"]):
-        message(version)
-    elif switch(["?h","?help","?halp"]):
-        message("Available commands: ?courseoftheweek, ?desc, ?randomcourse, ?goldmine, ?version, ?help, ?welcome. Shorts work too: ?cotw, ?rc, ?gm, etc. Do ?help <fullcommandname> to know more. Eg: ?help desc")
-    elif switch(["?w","?welcome"]):
-        message("Welcome to #learninghub! Here you'll find lots of resources and people to learn hacking/pentesting as well as other IT subjects. Type ?goldmine to get started. Type ?desc <coursenumber> to know the description of a course. You have to use the course number in the ghostbin. Type ?help for more.")
-    elif switch(["?h desc","?help desc"]):
-        message("This command will provide a description on a given course. Usage: ?desc <coursenumber>. Eg: ?desc 1 - Courses go from 0, 1, 2 and upwards. The course identifier can be found in the ?goldmine")
-    elif switch(["what of case"]):
-        message("Henry Dorsett Case is a low-level hustler in the underworld of Chiba City, Japan. Once a talented computer hacker, Case was caught stealing from his employer. As punishment, Case's nervous system was damaged with a mycotoxin, leaving him unable to access the global computer network in cyberspace. Unemployable, addicted to drugs, and suicidal, Case desperately searches the Chiba black clinics for a miracle cure.")
-    elif switch(["?try","what of try"]):
-        message("Do or Do not. There is no try.")
-    elif switch(["?luna"]):
-        message("When I admire the beauty of Luna, my soul expands in the worship of the creator.")
-    elif switch(["?multithr3d","what of multithr3d"]):
-        message("May your htop stats be low and your beards grow long.")
-    elif switch(["what of luna"]):
-        message("Luna is the way, Luna is the light. She's one of a kind, a sight for sore eyes.")
-    elif switch(["?anakin","what of anakin"]):
-        message("You were supposed to bring glory to the navy, not destroy it you faggot")
-    elif switch(["?claudiad","what of claudiad"]):
-        message("Tearing apart people's souls since forever. She also rides a unicorn.")
-    elif switch(["?niggers","what of niggers"]):
-        message("They smell.")
-    elif switch(["?h courseoftheweek","?help courseoftheweek"]):
-        message("This command will print the course decided for the week. Join us :)")
-    elif switch(["?h randomcourse","?help randomcourse"]):
-        message("This command will print a random course from the goldmine, in case you want to do one and can't decide")
-    elif switch(["?h goldmine","?help goldmine"]):
-        message("This command will print the goldmine with the full list of courses. Most of them are extracted from pluralsight, others are provided by anons.")
-    if senderuser in admins:
-        if switch(["?updatecourselist"]):
-            courses = {}
-            descriptions = {}
-            execfile(coursefile, courses)
-            execfile(descriptionsfile, descriptions)
+
+def write_data(data, filename):
+    w = open(filename, "w")
+    json.dump(data, w)
+
+
+def adduser(nick, user):
+    if user in data["admins"]:
+        data["users"].append(bot.sha2(nick))
+        write_data(data, CONF_FILENAME)
+        return "%s has been added to database." % nick
+    else:
+        return "Only bot admins can add users to the database."
+
+
+def get_argument(msg):
+    m = msg.split()
+    a = m[1] if len(m) > 1 else None
+    return a
+
+
+def exec_command(cmd, arg):
+    try:
+        c = str(data["defs"][cmd])
+        response = globals()[c](arg) if arg else globals()[c]()
+    except:
+        response = "That is not a valid command format."
+
+    return response
+
+
+def listen_irc():
+    while True:
+        user, msg, chan = bot.listen()
+        arg = get_argument(msg)
+
+        if msg == "?welcome" or msg == "?w":
+            welcome(user)
+        elif msg and msg[0] == '?':
+            cmd = msg[1:].split()[0].lower()
+
+            if cmd == "adduser" or cmd == "add":
+                nick = get_argument(msg)
+                response = adduser(nick, user)
+            elif cmd in data["commands"]:
+                response = data["commands"][cmd]
+                if arg:
+                    response = "%s: %s" % (arg, response)
+            elif cmd in data["defs"]:
+                response = exec_command(cmd, arg)
+            else:
+                response = "The command you are trying to execute does not exist."
+
+            bot.message(response, chan) if response else None
+
+
+def main():
+    print(art)
+    bot.auth()
+    bot.ping()
+    bot.join()
+    threading.Thread(target=chat).start()
+    threading.Thread(target=jew).start()
+    print("[+] Bot is up and working.\n")
+    print(conf["chans"][0])
+    print()
+    listen_irc()
+
+
+if __name__ == "__main__":
+    main()
